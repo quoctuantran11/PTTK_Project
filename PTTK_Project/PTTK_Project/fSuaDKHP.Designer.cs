36a63ee9fe73cfff7e2615926985f7ba83d7bcd1
Quoc Tuan <shinichihattori99@gmail.com>
2021-04-29 00:08:34 +0700

    minor changes

--- PTTK_Project/PTTK_Project/fSuaDKHP.Designer.cs
+++ PTTK_Project/PTTK_Project/fSuaDKHP.Designer.cs
@@ -36,13 +36,22 @@ namespace PTTK_Project
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(40, 106);
+            this.label1.Location = new System.Drawing.Point(39, 393);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 31);
             this.label1.TabIndex = 0;
@@ -52,17 +61,17 @@ namespace PTTK_Project
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(40, 43);
+            this.label2.Location = new System.Drawing.Point(39, 335);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(174, 31);
+            this.label2.Size = new System.Drawing.Size(184, 31);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Mã chứng chỉ";
+            this.label2.Text = "Tên chứng chỉ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(40, 165);
+            this.label3.Location = new System.Drawing.Point(40, 451);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(252, 31);
             this.label3.TabIndex = 2;
@@ -71,7 +80,7 @@ namespace PTTK_Project
             // btnHoanTat
             // 
             this.btnHoanTat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnHoanTat.Location = new System.Drawing.Point(315, 249);
+            this.btnHoanTat.Location = new System.Drawing.Point(315, 519);
             this.btnHoanTat.Name = "btnHoanTat";
             this.btnHoanTat.Size = new System.Drawing.Size(168, 53);
             this.btnHoanTat.TabIndex = 3;
@@ -80,31 +89,121 @@ namespace PTTK_Project
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(315, 43);
+            this.textBox1.Location = new System.Drawing.Point(315, 335);
             this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(408, 31);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(315, 106);
+            this.textBox2.Location = new System.Drawing.Point(315, 393);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(408, 31);
             this.textBox2.TabIndex = 5;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(315, 172);
+            this.dateTimePicker1.Location = new System.Drawing.Point(315, 451);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(407, 31);
             this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(85, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(586, 46);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sửa kết quả đăng ký học phần";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(315, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(408, 31);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(40, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tên học viên";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(40, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Khóa";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(40, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 31);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Năm";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(315, 279);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(408, 31);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(39, 279);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(179, 31);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Tên học phần";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(315, 167);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 33);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(315, 221);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(127, 33);
+            this.comboBox2.TabIndex = 15;
             // 
             // fSuaDKHP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 333);
+            this.ClientSize = new System.Drawing.Size(767, 600);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -128,5 +227,14 @@ namespace PTTK_Project
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
---
+++ PTTK_Project/PTTK_Project/fThongKeHocVien.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PTTK_Project
+{
+    partial class fThongKeHocVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbNam = new System.Windows.Forms.ComboBox();
+            this.cmbQuy = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 145);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(999, 507);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cmbNam
+            // 
+            this.cmbNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.cmbNam.FormattingEnabled = true;
+            this.cmbNam.Location = new System.Drawing.Point(714, 90);
+            this.cmbNam.Name = "cmbNam";
+            this.cmbNam.Size = new System.Drawing.Size(140, 37);
+            this.cmbNam.TabIndex = 1;
+            this.cmbNam.Text = "Năm";
+            // 
+            // cmbQuy
+            // 
+            this.cmbQuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.cmbQuy.FormattingEnabled = true;
+            this.cmbQuy.Location = new System.Drawing.Point(904, 90);
+            this.cmbQuy.Name = "cmbQuy";
+            this.cmbQuy.Size = new System.Drawing.Size(140, 37);
+            this.cmbQuy.TabIndex = 2;
+            this.cmbQuy.Text = "Quý";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(119, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thống kê số lượng học viên theo năm/quý";
+            // 
+            // fThongKeHocVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 679);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbQuy);
+            this.Controls.Add(this.cmbNam);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "fThongKeHocVien";
+            this.Text = "fThongKeHocVien";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbNam;
+        private System.Windows.Forms.ComboBox cmbQuy;
+        private System.Windows.Forms.Label label1;
+    }
+}
