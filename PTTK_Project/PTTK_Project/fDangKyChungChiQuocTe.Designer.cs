b1c4569e07c9d876966fca53b2956ac4dfb126ba
Quoc Tuan <shinichihattori99@gmail.com>
2021-04-28 20:56:06 +0700

    minor

--- PTTK_Project/PTTK_Project/fDangKyChungChiQuocTe.Designer.cs
+++ PTTK_Project/PTTK_Project/fDangKyChungChiQuocTe.Designer.cs
@@ -47,7 +47,6 @@ namespace PTTK_Project
             this.label10 = new System.Windows.Forms.Label();
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label2
@@ -56,7 +55,7 @@ namespace PTTK_Project
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label2.Location = new System.Drawing.Point(28, 79);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.Size = new System.Drawing.Size(94, 31);
             this.label2.TabIndex = 1;
             this.label2.Text = "Họ tên";
             // 
@@ -66,7 +65,7 @@ namespace PTTK_Project
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label3.Location = new System.Drawing.Point(28, 127);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.Size = new System.Drawing.Size(113, 31);
             this.label3.TabIndex = 2;
             this.label3.Text = "Giới tính";
             // 
@@ -76,7 +75,7 @@ namespace PTTK_Project
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label4.Location = new System.Drawing.Point(28, 174);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.Size = new System.Drawing.Size(135, 31);
             this.label4.TabIndex = 3;
             this.label4.Text = "Ngày sinh";
             // 
@@ -86,7 +85,7 @@ namespace PTTK_Project
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label5.Location = new System.Drawing.Point(28, 222);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.Size = new System.Drawing.Size(96, 31);
             this.label5.TabIndex = 4;
             this.label5.Text = "CMND";
             // 
@@ -94,9 +93,9 @@ namespace PTTK_Project
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label6.Location = new System.Drawing.Point(416, 174);
+            this.label6.Location = new System.Drawing.Point(416, 131);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.Size = new System.Drawing.Size(114, 31);
             this.label6.TabIndex = 5;
             this.label6.Text = "Ngày thi";
             // 
@@ -116,7 +115,7 @@ namespace PTTK_Project
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label8.Location = new System.Drawing.Point(416, 76);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(98, 17);
+            this.label8.Size = new System.Drawing.Size(184, 31);
             this.label8.TabIndex = 8;
             this.label8.Text = "Tên chứng chỉ";
             // 
@@ -126,7 +125,7 @@ namespace PTTK_Project
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(139, 118);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(115, 23);
+            this.comboBox1.Size = new System.Drawing.Size(115, 37);
             this.comboBox1.TabIndex = 10;
             // 
             // dateTimePicker1
@@ -134,15 +133,15 @@ namespace PTTK_Project
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.dateTimePicker1.Location = new System.Drawing.Point(139, 168);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(226, 21);
+            this.dateTimePicker1.Size = new System.Drawing.Size(226, 35);
             this.dateTimePicker1.TabIndex = 11;
             // 
             // dateTimePicker2
             // 
             this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.dateTimePicker2.Location = new System.Drawing.Point(543, 170);
+            this.dateTimePicker2.Location = new System.Drawing.Point(543, 127);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(236, 21);
+            this.dateTimePicker2.Size = new System.Drawing.Size(236, 35);
             this.dateTimePicker2.TabIndex = 12;
             // 
             // textBox1
@@ -150,7 +149,7 @@ namespace PTTK_Project
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.textBox1.Location = new System.Drawing.Point(139, 76);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(226, 21);
+            this.textBox1.Size = new System.Drawing.Size(226, 35);
             this.textBox1.TabIndex = 13;
             // 
             // textBox2
@@ -158,7 +157,7 @@ namespace PTTK_Project
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.textBox2.Location = new System.Drawing.Point(139, 218);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(226, 21);
+            this.textBox2.Size = new System.Drawing.Size(226, 35);
             this.textBox2.TabIndex = 14;
             // 
             // textBox5
@@ -166,7 +165,7 @@ namespace PTTK_Project
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.textBox5.Location = new System.Drawing.Point(543, 73);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(236, 21);
+            this.textBox5.Size = new System.Drawing.Size(236, 35);
             this.textBox5.TabIndex = 17;
             // 
             // label1
@@ -175,7 +174,7 @@ namespace PTTK_Project
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(186, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(393, 36);
+            this.label1.Size = new System.Drawing.Size(752, 67);
             this.label1.TabIndex = 0;
             this.label1.Text = "Đăng ký chứng chỉ quốc tế";
             // 
@@ -184,7 +183,7 @@ namespace PTTK_Project
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.textBox6.Location = new System.Drawing.Point(139, 311);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(226, 21);
+            this.textBox6.Size = new System.Drawing.Size(226, 35);
             this.textBox6.TabIndex = 32;
             // 
             // label10
@@ -193,7 +192,7 @@ namespace PTTK_Project
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label10.Location = new System.Drawing.Point(28, 315);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(51, 17);
+            this.label10.Size = new System.Drawing.Size(97, 31);
             this.label10.TabIndex = 31;
             this.label10.Text = "Địa chỉ";
             // 
@@ -202,7 +201,7 @@ namespace PTTK_Project
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.textBox7.Location = new System.Drawing.Point(139, 263);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(226, 21);
+            this.textBox7.Size = new System.Drawing.Size(226, 35);
             this.textBox7.TabIndex = 30;
             // 
             // label11
@@ -211,26 +210,16 @@ namespace PTTK_Project
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label11.Location = new System.Drawing.Point(28, 263);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(91, 17);
+            this.label11.Size = new System.Drawing.Size(171, 31);
             this.label11.TabIndex = 29;
             this.label11.Text = "Số điện thoại";
             // 
-            // comboBox2
-            // 
-            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(543, 118);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(236, 23);
-            this.comboBox2.TabIndex = 34;
-            // 
             // fDangKyChungChiQuocTe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(813, 412);
-            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.textBox7);
@@ -276,6 +265,5 @@ namespace PTTK_Project
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
